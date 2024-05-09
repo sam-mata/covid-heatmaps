47bcd2bbe1b2769a9c4520ceac9f54544d31afe1
Sam <sammatanz@gmail.com>
2024-05-09 14:11:51 +1200

    Project Structure Finalised

--- app.vue
+++ app.vue
@@ -1,7 +1,16 @@
 <script setup>
 import * as Plot from "@observablehq/plot";
+import PlotFigure from "./PlotFigure.js";
+import penguins from "./data/penguins.json";
 </script>
 
 <template>
-  <h1 class="text-xl text-blue-500">Test Text</h1>
+  <div class="mx-auto w-fit py-24">
+    <h1 class="text-5xl font-bold text-blue-700">Plot + Nuxt Works Bozos</h1>
+    <PlotFigure :options="{
+      marks: [
+        Plot.dot(penguins, { x: 'culmen_length_mm', y: 'culmen_depth_mm' }),
+      ],
+    }" />
+  </div>
 </template>
