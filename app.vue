2b064e4c21ca7a5ae6c8f5a1d9291ee7b9217412
Sam <sammatanz@gmail.com>
2024-05-22 13:48:26 +1200

    Basic Site Layout Completed
    - Non-functional selectors for feature and date range implemented.
    - Placeholder structure complete
    - Placeholder graph implemented with layout

--- app.vue
+++ app.vue
@@ -2,15 +2,56 @@
 import * as Plot from "@observablehq/plot";
 import PlotFigure from "./PlotFigure.js";
 import penguins from "./data/penguins.json";
+const features = ['Deaths', 'Vaccinations', 'Cases'], feature = ref(features[0]);
+
+import { ref } from 'vue';
+
+const range = ref({
+  start: new Date(2020, 0, 6),
+  end: new Date(2020, 0, 10),
+});
 </script>
 
 <template>
-  <div class="mx-auto w-fit py-24">
-    <h1 class="text-5xl font-bold text-blue-700">Plot + Nuxt Works Bozos</h1>
-    <PlotFigure :options="{
-      marks: [
-        Plot.dot(penguins, { x: 'culmen_length_mm', y: 'culmen_depth_mm' }),
-      ],
-    }" />
-  </div>
+  <UContainer>
+    <UCard>
+      <template #header>
+        <h1 class="text-6xl">
+          Covid-19 Visualiser
+        </h1>
+      </template>
+      <UCardBody>
+        <div class="flex">
+          <div class="w-1/4">
+            <h2 class="font-extrabold text-3xl py-2">Map Settings</h2>
+            <h3 class="py-2 font-semibold">Mapping:</h3>
+            <USelect v-model="feature" :options="features" />
+            <UDivider class="py-4" />
+            <h3 class="py-2 font-semibold">Date Range:</h3>
+            <VDatePicker v-model.range="range" mode="date" />
+          </div>
+          <UDivider orientation="vertical" class="px-4" />
+          <div>
+            <h2 class="font-extrabold text-3xl py-2">Map</h2>
+            <div class=" mx-auto w-fit p-4">
+              <PlotFigure :options="{
+                marks: [
+                  Plot.dot(penguins, { x: 'culmen_length_mm', y: 'culmen_depth_mm' }),
+                ],
+              }" />
+            </div>
+          </div>
+        </div>
+      </UCardBody>
+      <template #footer>
+        <p>
+          All work completed by Zoe Picone, Nathan Bridge-Earney, and Sam Mata.
+        </p>
+        <p>
+          Project code available <ULink href="https://github.com/sam-mata/covid-visualiser">here</ULink>.
+        </p>
+      </template>
+    </UCard>
+  </UContainer>
+
 </template>
