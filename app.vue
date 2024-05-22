77468dfabd0b5b44e8b3b0ea531707e575d180dc
Sam <sammatanz@gmail.com>
2024-05-22 16:32:29 +1200

    Improved Code Readability
    - Exported common functionality into separate component
    - Commented current code
    - Fixed `README.md` formatting

--- app.vue
+++ app.vue
@@ -2,38 +2,48 @@
 import * as Plot from "@observablehq/plot";
 import PlotFigure from "./PlotFigure.js";
 import penguins from "./data/penguins.json";
-const features = ['Deaths', 'Vaccinations', 'Cases'], feature = ref(features[0]);
-
-import { ref } from 'vue';
-
-const range = ref({
-  start: new Date(2020, 0, 6),
-  end: new Date(2020, 0, 10),
-});
 import WorldMap from "~/components/WorldMap.ts";
+
+
+const features = ['Deaths', 'Vaccinations', 'Cases'], feature = ref(features[0]);
+const range = ref({ start: new Date(2020, 0, 1), end: new Date(2023, 11, 31), });
 </script>
 
 <template>
   <UContainer>
-    <UCard>
+    <UCard class="h-screen">
+
+      <!-- Header -->
       <template #header>
-        <h1 class="text-6xl">
+        <h1 class="text-6xl font-bold text-center py-6">
           Covid-19 Visualiser
         </h1>
       </template>
+
+      <!-- Body -->
       <UCardBody>
         <div class="flex">
+
+          <!-- Map Settings -->
           <div class="w-1/4">
-            <h2 class="font-extrabold text-3xl py-2">Map Settings</h2>
-            <h3 class="py-2 font-semibold">Mapping:</h3>
+            <h2 class="font-bold text-3xl py-2 text-center">Settings</h2>
+            <!-- Feature Selection -->
+            <SelectorSection title="Feature" icon="check-circle-solid" />
             <USelect v-model="feature" :options="features" />
+
             <UDivider class="py-4" />
-            <h3 class="py-2 font-semibold">Date Range:</h3>
+
+            <!-- Date Range Selection -->
+            <SelectorSection title="Date" icon="calendar-days-16-solid" />
             <VDatePicker v-model.range="range" mode="date" />
           </div>
+
           <UDivider orientation="vertical" class="px-4" />
+
+          <!-- Map -->
           <div>
-            <h2 class="font-extrabold text-3xl py-2">Map</h2>
+            <h2 class="font-bold text-3xl py-2 text-center">Map</h2>
+            <!-- Plot-->
             <div class=" mx-auto w-fit p-4">
               <PlotFigure :options="{
                 marks: [
@@ -42,14 +52,19 @@ import WorldMap from "~/components/WorldMap.ts";
               }" />
             </div>
           </div>
+
         </div>
       </UCardBody>
+
+      <!-- Footer -->
       <template #footer>
-        <p>
+        <p class="text-center pt-3">
           All work completed by Zoe Picone, Nathan Bridge-Earney, and Sam Mata.
         </p>
-        <p>
-          Project code available <ULink href="https://github.com/sam-mata/covid-visualiser">here</ULink>.
+        <p class="text-center">
+          Project code available <ULink href="https://github.com/sam-mata/covid-visualiser"
+            class="underline underline-offset-2">here
+          </ULink>.
         </p>
       </template>
     </UCard>
---
+++ components/SelectorSection.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="flex items-center">
+        <UIcon :name="`i-heroicons-${icon}`" class="pr-6" />
+        <h3 class="py-2 font-semibold">{{ title }} :</h3>
+    </div>
+</template>
+
+<script setup>
+import { defineProps } from 'vue';
+
+const props = defineProps({
+    title: {
+        type: String,
+        required: true,
+    },
+    icon: {
+        type: String,
+        required: true,
+    },
+});
+</script>
