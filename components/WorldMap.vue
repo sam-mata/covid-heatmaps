05cf79b29d7bdb50cda48036e602430cdbc8926a
Zoe Picone <zoe@zoepicone.dev>
2024-05-27 15:00:02 +1200

    refactor: remove superfluous import

--- components/WorldMap.vue
+++ components/WorldMap.vue
@@ -1,7 +1,6 @@
 <script setup lang="ts">
 import * as Plot from "@observablehq/plot";
 import PlotFigure from "../PlotFigure.js";
-import {h} from "vue";
 import world from "../data/countries-110m.json"
 import * as topojson from "topojson-client"
 
